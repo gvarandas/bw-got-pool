ac4cfd87d77eda66f79b2ca221e17923d9563ec0
Guilherme <jgvarandas@gmail.com>
2019-04-13 01:40:17 -0400

    Removing logs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import { CharactersContext } from "../containers/CharacterContext";
 import CharacterGrade from "../components/CharacterGrade";
 
 const HomePage = ({ username = "" }) => {
-  console.log("userName", username);
   const { dispatch } = useContext(CharactersContext);
 
   useEffect(() => {
